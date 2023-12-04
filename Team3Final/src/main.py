6530348cb8f7e03fd39259e8e7bf34c585e7ad3d
IshaanP <90219558+BesottedP@users.noreply.github.com>
2023-12-03 22:17:23 -0500

    Teleop Code

---
+++ Team3Final/src/main.py
@@ -0,0 +1,33 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       final.py                                                      #
+# 	Author:       Ishaan                                                       #
+# 	Created:      12/3/2023, 6:48:36 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+from math import *
+
+#Instantiating Devices
+brain = Brain()
+controller = Controller()
+
+#Instantiating motors
+frontLeft_motor = Motor(Ports.PORT10, 18_1, True)
+frontRight_motor = Motor(Ports.PORT1, 18_1, False)
+backLeft_motor = Motor(Ports.PORT10, 18_1, True)
+backRight_motor = Motor(Ports.PORT10, 18_1, True)
+
+while True:
+    frontLeft_motor.set_velocity(2* (controller.axis3.position()+controller.axis1.position()), RPM)
+    frontRight_motor.set_velocity(2* (controller.axis3.position()-controller.axis1.position()), RPM)
+    backLeft_motor.set_velocity(2* (controller.axis3.position()+controller.axis1.position()), RPM)
+    backRight_motor.set_velocity(2* (controller.axis3.position()-controller.axis1.position()), RPM)
+
+
+
+
+        
