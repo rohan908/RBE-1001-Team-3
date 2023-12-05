fed4f5eb084f73b300dac81f78b6caf38c3705fb
IshaanP <90219558+BesottedP@users.noreply.github.com>
2023-12-04 20:30:24 -0500

    Added code for intake

--- Team3Final/src/main.py
+++ Team3Final/src/main.py
@@ -20,6 +20,7 @@ frontLeft_motor = Motor(Ports.PORT12, 18_1, True)
 frontRight_motor = Motor(Ports.PORT13, 18_1, False)
 backLeft_motor = Motor(Ports.PORT11, 18_1, True)
 backRight_motor = Motor(Ports.PORT20, 18_1, False)
+intake_motor = Motor(Ports.PORT15, 18_1, True)
 
 while True:
     frontLeft_motor.set_velocity(2* (controller.axis3.position()+controller.axis1.position()), RPM)
@@ -30,6 +31,13 @@ while True:
     frontRight_motor.spin(FORWARD)
     backLeft_motor.spin(FORWARD)
     backRight_motor.spin(FORWARD)
+    if controller.buttonL1.pressing():
+        intake_motor.spin(FORWARD, 200, RPM)
+    elif controller.buttonL2.pressing():
+        intake_motor.spin(REVERSE, 200, RPM)
+    elif controller.buttonB.pressing():
+        intake_motor.spin(FORWARD, 0, RPM)
+        
 
 
 
